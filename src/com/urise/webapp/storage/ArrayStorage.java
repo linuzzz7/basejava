caef3455c044f67c90ce893f533912bf3e8d6ff1
Alexander Zukov <linuzzz7@yandex.ru>
2021-04-04 22:10:32 +0300

    resume lesson 2 fixed

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,62 +8,62 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10_000];
     private int size = 0;
-    private int checkResumeId;
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    public boolean checkResume(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                checkResumeId = i;
-                return true;
+    private int checkResume(String uuid) {
+        for (int index = 0; index < size; index++) {
+            if (storage[index].getUuid().equals(uuid)) {
+                return index;
             }
         }
-        return false;
+        return -1;
     }
 
     public void save(Resume newResume) {
-        if (size == 10000) {
+        if (size == storage.length) {
             System.out.println("База данных резюме заполнена, добавление не возможно");
         } else {
-            if (!checkResume(newResume.getUuid())) {
+            if (checkResume(newResume.getUuid()) == -1) {
                 size++;
                 storage[size - 1] = newResume;
             } else {
-                System.out.println("Резюме уже существует");
+                System.out.println("Резюме " + newResume.getUuid() + " уже существует");
             }
         }
     }
 
     public Resume get(String uuid) {
-        if (checkResume(uuid)) {
-            return storage[checkResumeId];
-        } else {
-            System.out.println("Резюме не найдено");
-            return null;
+        int index = checkResume(uuid);
+        if (index != -1) {
+            return storage[index];
         }
+        System.out.println("Резюме " + uuid + " не найдено");
+        return null;
     }
 
     public void delete(String uuid) {
-        if (checkResume(uuid)) {
-            storage[checkResumeId] = storage[size - 1];
+        int index = checkResume(uuid);
+        if (index != -1) {
+            storage[index] = storage[size - 1];
             storage[size - 1] = null;
             size--;
         } else {
-            System.out.println("Резюме не найдено, удаление не произведено");
+            System.out.println("Резюме " + uuid + " не найдено, удаление не произведено");
         }
     }
 
     public void update(Resume resume) {
-        if (checkResume(resume.getUuid())) {
-            storage[checkResumeId] = resume;
+        int index = checkResume(resume.getUuid());
+        if (index != -1) {
+            storage[index] = resume;
         } else {
-            System.out.println("Резюме не найдено, обновление не произведено");
+            System.out.println("Резюме " + resume.getUuid() + " не найдено, обновление не произведено");
         }
     }
 
